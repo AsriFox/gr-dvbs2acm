5efad06ec010924c86ac135c894fb298a612db91
AsriFox <asrifox@yandex.ru>
2023-05-04 14:06:24 +0300

    Changed blockname.h template to GR 3.x format; Temporarily disabled pybind generation

--- blocklib/dvbs2acm/bbheader_bb/bbheader_bb_cpu.cc
+++ blocklib/dvbs2acm/bbheader_bb/bbheader_bb_cpu.cc
@@ -12,18 +12,17 @@
 #include "bbheader_bb_cpu_gen.h"
 
 namespace gr {
-namespace gr - dvbs2acm
+namespace dvbs2acm {
+
+bbheader_bb_cpu::bbheader_bb_cpu(block_args args) : INHERITED_CONSTRUCTORS {}
+
+work_return_t bbheader_bb_cpu::work(work_io& wio)
 {
-
-    bbheader_bb_cpu::bbheader_bb_cpu(block_args args) : INHERITED_CONSTRUCTORS {}
-
-    work_return_t bbheader_bb_cpu::work(work_io & wio)
-    {
-        // Do <+signal processing+>
-        // Block specific code goes here
-        return work_return_t::OK;
-    }
+    // Do <+signal processing+>
+    // Block specific code goes here
+    return work_return_t::OK;
+}
 
 
-} // namespace gr- dvbs2acm
+} // namespace dvbs2acm
 } // namespace gr
